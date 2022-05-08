f50fd942342c96c16d5a395f372b157e34a972be
Ahsan-Gazi <ahsangazi01@gmail.com>
2022-05-08 11:17:07 +0600

    get single data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const ObjectId = require('mongodb').ObjectId;
+ const ObjectId = require('mongodb').ObjectId;
 
 require('dotenv').config();
 const app = express();
@@ -36,7 +36,12 @@ async function run() {
             res.send(services);
         });
 
-        
+        app.get('/service/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
     }
     finally {
